22286554a8f0ead3885a93531b286e7b91585314
Code Sandbox <hello@codesandbox.io>
2023-06-21 17:13:04 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ad9bb5b1fbbf8d1fdcd55f99749f184c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e0650e581098f44c4d6faaaed5de570",
+    "url": "https://mamdouhreda.github.io/csb-7rr9hx/index.html"
+  },
+  {
+    "revision": "a2d12905c0b569165bba",
+    "url": "https://mamdouhreda.github.io/csb-7rr9hx/static/js/2.61ae90a2.chunk.js"
+  },
+  {
+    "revision": "a4bafc6d3aa970c43370",
+    "url": "https://mamdouhreda.github.io/csb-7rr9hx/static/js/main.0c3d0210.chunk.js"
+  },
+  {
+    "revision": "4688c5ecc89428ea9977",
+    "url": "https://mamdouhreda.github.io/csb-7rr9hx/static/js/runtime-main.7c4a7931.js"
+  }
+]);
